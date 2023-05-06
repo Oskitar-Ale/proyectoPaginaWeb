2a758c76ce912f952c9377088a2dafb00e090278
Oskitar-Ale <oscarvilca66@gmail.com>
2023-05-06 13:52:57 -0500

    3ra Actualizacion del html y css, nuevas imagenes

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu= document.querySelector(".container-menu");
+
+navToggle.addEventListener("click",() => {
+    navMenu.classList.toggle("nav-menu__visible");
+});
