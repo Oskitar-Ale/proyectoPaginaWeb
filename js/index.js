5d842dee0e714c4a011de37c444e4a4aee8985d8
Oskitar-Ale <oscarvilca66@gmail.com>
2023-05-12 23:59:40 -0500

    update

--- js/index.js
+++ js/index.js
@@ -7,3 +7,24 @@ navToggle.addEventListener("click",() => {
 
 
 
+const navbar = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('container-nav--scrolled');
+  } else {
+    navbar.classList.remove('container-nav--scrolled');
+  }
+});
+
+
+const movingImages = document.querySelectorAll('.murcielagos-volando');
+const container = document.querySelector('body');
+
+movingImages.forEach((img) => {
+  setInterval(() => {
+    const x = Math.floor(Math.random() * container.clientWidth);
+    const y = Math.floor(Math.random() * container.clientHeight);
+    img.style.transform = `translate(${x}px, ${y}px)`;
+  }, 10000);
+});
