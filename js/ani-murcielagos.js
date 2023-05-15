af28204cb510636e7d23d6596391e5123d9c5a4c
Oskitar-Ale <oscarvilca66@gmail.com>
2023-05-15 18:51:04 -0500

    update

--- js/ani-murcielagos.js
+++ js/ani-murcielagos.js
@@ -3,10 +3,11 @@
 const movingImages = document.querySelectorAll('.murcielagos-volando');
 const container = document.querySelector('body');
 
+
 movingImages.forEach((img) => {
   setInterval(() => {
-    const x = Math.floor(Math.random() * container.clientWidth);
-    const y = Math.floor(Math.random() * container.clientHeight);
+    const x = Math.floor(Math.random() * (container.clientWidth));
+    const y = Math.floor(Math.random() * (container.clientHeight));
     img.style.transform = `translate(${x}px, ${y}px)`;
   }, 10000);
 });
--- js/scroollTo.js
+++ js/scroollTo.js
@@ -1,32 +1,4 @@
 
-//mal deslizamiento
-// const inicio = document.querySelector("#inicio");
-// const quienesSomos = document.querySelector("#quienes-somos");
-// const productos = document.querySelector("#productos");
-// const promociones = document.querySelector("#promociones");
-// const servicios = document.querySelector("#servicios");
-// const contactos = document.querySelector("#contactos");
-
-
-// inicio.addEventListener("click", () => scrollToSection(0));
-// quienesSomos.addEventListener("click", () => scrollToSection(1));
-// productos.addEventListener("click", () => scrollToSection(2));
-// promociones.addEventListener("click", () => scrollToSection(3));
-// servicios.addEventListener("click", () => scrollToSection(4));
-// contactos.addEventListener("click", () => scrollToSection(5));
-
-
-// function scrollToSection(sectionIndex) {
-//   const sections = document.querySelectorAll("section");
-//   const targetSection = sections[sectionIndex];
-//   const targetPosition = targetSection.offsetTop;
-
-//   window.scrollTo({
-//     top: targetPosition,
-//     behavior: "smooth"
-//   });
-// }
-
 const enlacesNav = document.querySelectorAll("nav a");
 
 enlacesNav.forEach(enlace => {
